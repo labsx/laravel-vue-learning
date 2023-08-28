63dddca87c9d3883bc8c3a14ffa89dba5b2e5edc
labsx <jrlabrador@revlv.com>
2023-08-28 19:55:29 -0400

    learning laravel vue with crud

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::latest()->get();
+
+        return $users;
+    }
+    public function store()
+    {
+        request()->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users,email',
+            'password' => 'required|min:8',
+        ]);
+
+        return User::create([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => bcrypt(request('password')),
+        ]);
+    }
+    public function update(User $user)
+    {
+        request()->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users,email,'.$user->id,
+            'password' => 'sometimes|min:8',
+        ]);
+
+        $user->update([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => request('password') ? bcrypt(request('password')) : $user->password,
+        ]);
+
+        return $user;
+    }
+    public function delete(User $user)
+    {
+        $user->delete();
+
+        return response()->noContent();
+    }
+}
